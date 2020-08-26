2c9b5f8d19b5954ef969ace02b9789bf8aa6792f
shaikhov-i <65728594+shaikhov-i@users.noreply.github.com>
2020-08-26 16:33:52 +0300

    Practice JS. Tabs and time indicator completed

---
+++ js/script.js
@@ -0,0 +1,137 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+
+    // To usе
+
+    createTab('.info-header', '.info-header-tab', '.info-tabcontent');
+
+
+    // createTab(1, 2, 3);
+
+    // 1) Parent of tab headers
+    // 2) Heads of tabs
+    // 3) Tab content
+
+
+
+
+
+    // Example
+
+    // 1) parent of tab headers
+
+    // <div class="info-header"> 
+
+    // 2) Heads of tabs
+
+    //      <div class="info-header-tab"></div>
+    //      <div class="info-header-tab"></div>
+    //      <div class="info-header-tab"></div>
+    //      <div class="info-header-tab"></div>
+    // </div>
+
+    // 3) Tab content   
+
+    // <div class="info-tabcontent fade">
+    // <div class="info-tabcontent fade">
+    // <div class="info-tabcontent fade">
+    // <div class="info-tabcontent fade">
+
+
+
+
+
+
+
+
+    function createTab(infoHeader, headerTab, infoTab) {
+        let tab = document.querySelectorAll(headerTab),
+            tabContent = document.querySelectorAll(infoTab),
+            info = document.querySelector(infoHeader);
+
+
+
+        // Function for hiding tab contents
+
+        function hideTabContent(a) {
+            for (let i = a; i < tabContent.length; i++) {
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add('hide');
+            }
+        }
+
+        // Hiding all tab contents beside 1
+
+        hideTabContent(1);
+
+
+        // Function for showing tab content
+
+        function showTabContent(b) {
+            if (tabContent[b].classList.contains('hide')) {
+                tabContent[b].classList.remove('hide');
+                tabContent[b].classList.add('show');
+            }
+        }
+
+        info.addEventListener('click', function(event) {
+            let target = event.target;
+            if (target && target.classList.contains(headerTab.substring(1, ))) {
+                for (let i = 0; i < tab.length; i++) {
+                    if (target == tab[i]) {
+                        hideTabContent(0);
+                        showTabContent(i);
+                    }
+                }
+            }
+        })
+    }
+
+    // Timer
+
+    let deadline = '2020-09-30';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            if (t.total > 0) {
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+            }
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('timer', deadline);
+});
